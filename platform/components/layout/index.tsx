bcf9bb03fb1fe8ed8d7d0a016b2284ad82bb6ecb
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2024-06-22 23:15:22 +0300

    fix routing issue

--- platform/components/layout/index.tsx
+++ platform/components/layout/index.tsx
@@ -1,20 +1,21 @@
 "use client"
 import { DynamicContextProvider } from '@dynamic-labs/sdk-react-core';
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-import { redirect } from 'next/navigation';
+import { redirect, useRouter } from 'next/navigation';
 
 type P = {
     children: React.ReactNode
 }
 
 const Layout = ({children}:P) => {
+    const router = useRouter()
     return(<DynamicContextProvider
         settings={{
             handlers: {
                 handleVerifiedUser: async (args) => {
                     console.log("handleBeforeAuth was called", args);
                     localStorage.setItem("auth", args.user.sessionId);
-                    redirect("/chat");
+                    router.replace("/chat");
                 },
             },
             environmentId: process.env.NEXT_PUBLIC_DYNAMIC_KEY ?? "",
