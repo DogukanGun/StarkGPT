80cd6db179791335fcc805e9f8f85ea3ee3b2078
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2024-06-22 22:46:48 +0300

    fix build error

--- platform/app/layout.tsx
+++ platform/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Layout from "@/components/layout";
 import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
+import Navbar from "@/components/layout/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Theme>
           <Layout>
+            <Navbar />
             {children}
           </Layout>
         </Theme>
